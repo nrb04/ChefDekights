92458767387daa60e9cc01c990438edfc92b31d1
nrb04 <nredoyan@gmail.com>
2023-05-02 16:32:49 +0600

    bootstrap+others added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBROJ0xydcD7iHElFyDuPY1Cuiw5dgZ8Yk",
+  authDomain: "ass10-chef-delights.firebaseapp.com",
+  projectId: "ass10-chef-delights",
+  storageBucket: "ass10-chef-delights.appspot.com",
+  messagingSenderId: "843647166340",
+  appId: "1:843647166340:web:9092d4c455a8a75eac9fd1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/routes/.PrivateRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrivateRoute = () => {
+  return <div>skf;lsdkf;ls</div>;
+};
+
+export default PrivateRoute;
